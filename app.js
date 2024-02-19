b922c13fca24a6dfd735d1f2f2e4a39f741787cd
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-19 18:50:18 +0200

    006 Starting Our API_ Handling GET Requests

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'natours' });
-});
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
 
-app.post('/', (req, res) => {
-  res.status(201).json('You can post here...');
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(201).json('You can post here...');
+// });
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
