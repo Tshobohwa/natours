7b9a6904fbcd5ef539aa3e77c7f43818355b271f
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-20 21:09:05 +0200

    Remove comments

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
 module.exports = app;
