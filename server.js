9881bc54e5776b4762f943d92656c1d22e53c1a3
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-21 21:14:46 +0200

    Use dotenv

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const app = require('./app');
+const dotenv = require('dotenv')
+const app = require('./app')
 
-const port = 3000;
-
+dotenv.config({ path: './config.env' })
+const port = 3000
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
