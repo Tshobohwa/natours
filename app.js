1df231aff1a05b6540bf50cab26bae0363735435
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-21 21:13:44 +0200

    Add environment variables

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const tourRouter = require('./routes/tourRoutes');
 const app = express();
 
 app.use(express.json());
+
 app.use(express.static(`${__dirname}/public/`));
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use((req, res, next) => {
   req.createdAt = new Date().toISOString();
