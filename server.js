0c0e9c7b73971155956da11a91b6d05d1499c40a
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-22 18:00:28 +0200

    Change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const app = require('./app')
 
 dotenv.config({ path: './config.env' })
-const port = 3000
+const port = 8000
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
