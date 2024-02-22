7edf703c01685bde0484698eb0434d8fde6dcf5d
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-22 09:14:57 +0200

    Delete CR

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
 
-const userRouter = require('./routes/userRoutes');
-const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.static(`${__dirname}/public/`));
+app.use(express.static(`${__dirname}/public/`))
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
 app.use((req, res, next) => {
-  req.createdAt = new Date().toISOString();
-  next();
-});
+  req.createdAt = new Date().toISOString()
+  next()
+})
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/tours', tourRouter)
 
-module.exports = app;
+module.exports = app
