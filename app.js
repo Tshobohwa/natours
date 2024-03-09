82a72a891869cfecb6b829d59658e3f5841f9253
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-09 23:12:06 +0200

    006 Better Errors and Refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 
 const userRouter = require('./routes/userRoutes')
 const tourRouter = require('./routes/tourRoutes')
@@ -26,14 +27,6 @@ app.all('*', (req, res, next) => {
   next(err)
 })
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
-  next(err)
-})
+app.use(globalErrorHandler)
 
 module.exports = app
