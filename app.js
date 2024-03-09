21bcb909be2880d8a076aaedd8e1b2bde9f83fb1
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-09 22:10:04 +0200

    005 Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -21,11 +21,20 @@ app.use('/api/v1/users', userRouter)
 app.use('/api/v1/tours', tourRouter)
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Route ${req.originalUrl} not found`,
+  const err = new Error(`Route ${req.originalUrl} not found`)
+  err.statusCode = 404
+  err.status = 'fail'
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   })
-  next()
+  next(err)
 })
 
 module.exports = app
