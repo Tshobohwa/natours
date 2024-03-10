f372c7f35b483a94b9e0fe31b0bf0c1363441ee7
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-10 10:23:52 +0200

    009 Errors During Development vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+const handleErrorInProduction = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+  } else {
+    console.error('Error 💥', err)
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    })
+  }
+}
+
+const handleErrorInDevelopment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  })
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: 'Something went wrong!',
+  })
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
+  if (process.env.NODE_ENV === 'production') handleErrorInProduction(err, res)
+  else handleErrorInDevelopment(err, res)
   next(err)
 }
