b9b28fd36321376eaf984bee712c25fbb7645c9e
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-13 01:17:33 +0200

    004 Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 userSchema = new mongoose.Schema({
   name: {
@@ -21,16 +22,31 @@ userSchema = new mongoose.Schema({
     minlength: [8, 'Password must have at least 8 characters'],
     maxlength: [32, 'Password must have at most 32 characters'],
   },
-  passordConfirm: {
+  passwordConfirm: {
     type: String,
     required: true,
     validate: {
+      // This only works on CREATE and SAVE!!!
       validator: function (val) {
         return this.password === val
       },
-      message: 'password confirm must be equal to password',
+      message: 'password are not the same',
     },
   },
+})
+
+userSchema.pre('save', async function (next) {
+  console.log(this)
+  //Only run this function if password was modified
+  if (!this.isModified('password')) return next()
+
+  // Hash the password with cost of 12 (how CPU intesive the operation will be)
+  this.password = await bcrypt.hash(this.password, 12)
+
+  // delete the password confirmed field
+  this.passwordConfirm = undefined
+
+  next()
 })
 
 const User = mongoose.model('User', userSchema)
