46d6d7a28b056c4b9531f8d4775d999011641044
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-14 22:38:23 +0200

    011 Authorization_ User Roles and Permissions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,6 +20,10 @@ router
   .route('/:id')
   .get(toursController.getTour)
   .patch(toursController.updateTour)
-  .delete(toursController.deleteTour)
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    toursController.deleteTour,
+  )
 
 module.exports = router
